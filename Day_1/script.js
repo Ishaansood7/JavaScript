efe613f88ba71888270a12ba32643b597886ecbe
Ishaan Sood <ishaansood7@gmail.com>
2026-07-01 12:00:20 +0530

    Add files via upload

---
+++ Day_1/script.js
@@ -0,0 +1,35 @@
+// Printing my Name
+let myName = "Ishaan Sood";
+console.log(myName);
+
+let myAge = 20;
+console.log(myAge);
+
+let favHobby = "Coding";
+console.log(favHobby);
+
+console.log("Hello, my name is " + myName + ". I am " + myAge + " years old and my favorite hobby is " + favHobby + ".");
+
+// console.log(`Hello, my name is ${myName}. I am ${myAge} years old and my favorite hobby is ${favHobby}.`);
+
+console.log("I am learning Javascript");
+
+// Printing the sum of two numbers
+let a = 10;
+let b = 20;
+console.log(a+b);
+
+let result = a + b;
+
+a = 100;
+b = 45;
+let result2 = a - b;
+
+// console.log("The sum of " + a + " and " + b + " is: " + result);
+ // console.log(`The sum of ${a} and ${b} is: ${result}`);
+// console.log("The sum of " + a + " and " + b + " is: " + (a+b));
+// console.log(`The sum of ${a} and ${b} is: ${a+b}`);
+console.log(result);
+console.log(result2);
+
+
