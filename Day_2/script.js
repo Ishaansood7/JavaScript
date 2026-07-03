e25b5da3e2484f2cc1d2c000ed08908a80ba3d57
Ishaan Sood <ishaansood7@gmail.com>
2026-07-03 10:13:16 +0530

    Add files via upload

---
+++ Day_2/script.js
@@ -0,0 +1,36 @@
+// Datatypes in JavaScript
+let language = "JavaScript";
+const year = 1995;
+let isFun = true;
+
+console.log(language);
+console.log(year);
+console.log(isFun);
+
+console.log(typeof language);
+console.log(typeof year);
+console.log(typeof isFun);
+
+// Easy
+let firstName = "Ishaan";
+let age = 20;
+let isCoding = true;
+
+console.log(firstName);
+console.log(age);
+console.log(isCoding);
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isCoding);
+
+//Medium
+const country = "India";
+let city = "New Delhi";
+
+console.log(country);
+console.log(city);
+
+city = "Ludhiana";
+console.log(city);
+
